e3acba7a44cba670a45c2a0091972f734f2996f3
StasSkv <stas000123@gmail.com>
2025-07-09 09:46:35 +0300

    add avtoLoginAfterRegister

--- src/controllers/auth/auth.ts
+++ src/controllers/auth/auth.ts
@@ -10,6 +10,22 @@ import { ISession } from '../../db/models/session/session.js';
 
 export const registerUserController = async (req: Request, res: Response) => {
   const user = await registerUser(req.body);
+  const session = await loginUser(req.body);
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + SEVEN_DAYS),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + SEVEN_DAYS),
+  });
+  res.status(200).json({
+    status: 200,
+    message: 'User logged in successfully',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
   res.status(201).json({
     status: 201,
     message: 'User registered successfully',
